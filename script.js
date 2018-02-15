5bc40dc11814621545e6fdc550eec0dbc786a022
Kyle White <kwdwhite@yahoo.com>
2018-02-15 16:16:30 -0500

    almost

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ function putInHtml(answer, location) {
 }
 // putInHtml(name(), 'div2');
 
-
-
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 var total = 0;
 
@@ -88,43 +86,133 @@ for(var i = 10; i > 0; i--) {
 }
 putInHtml(phat, 'div10');
 
+
+
 // //#11
-// for(i = 0; i < sampleArray.length; i++) {
-//     hit = sampleArray + " ";
-//     // putInHtml(sampleArray[i] + ' ');
-// }
-// putInHtml(hit, 'div11');
+for(i = 0; i < sampleArray.length; i++) {
+    hit = sampleArray + " ";
+    // putInHtml(sampleArray[i] + ' ');
+}
+putInHtml(hit, 'div11');
 
 // //#12
-// document.write('<br>');
-// for(i = 0; i < sampleArray.length; i++) {
-//     if ((i % 2) === 0)
-//     document.write(sampleArray[i] + ' ');
-// }
+var ThatLi = "";
+for(i = 0; i < sampleArray.length; i++) {
+    if (sampleArray[i] % 2 === 0)
+    ThatLi = ThatLi + sampleArray[i] + " ";
+    
+}
+putInHtml(ThatLi, 'div12');
+
 // //#13
-// document.write('<br>');
+var ThatLig = "";
+for(i = 0; i < sampleArray.length; i++) {
+    if (sampleArray[i] % 2 !== 0)
+    ThatLig = ThatLig + sampleArray[i] + " ";
+
+}
+putInHtml(ThatLig, 'div13');
+
 // //#14
+var ThatLigh = "";
+for(i = 0; i < sampleArray.length; i++) {
+    ThatLigh = ThatLigh + (sampleArray[i] * sampleArray[i]) + " ";
+}
+putInHtml(ThatLigh, 'div14');
 
 // //#15
-var deeDuh = "";
-for(i = 1; i < 21; i++) {
-    deeDuh = deeDuh + i + " ";
+var fuj = 0;
+for(i = 0; i < 21; i++) {
+    fuj = fuj + i;
  }
- putInHtml(deeDuh, 'div15');
+ putInHtml(fuj, 'div15');
+
 // //#16
-// document.write('<br>');
-// for(var i = 0; i < sampleArray.length; i++){
-//     total = (total + sampleArray[i])
-//     document.write(total);
-// }
+var ThatLight = 0;
+for(i = 0; i < sampleArray.length; i++) {
+    ThatLight = ThatLight + sampleArray[i];
+}
+putInHtml(ThatLight, 'div16');
+
 //#17
-// var fud = Math.min(sampleArray.every);
-// putInHtml(fud, 'div17');
+function myArrayMin(arr) {
+    return Math.min.apply(null,arr);
+}
+putInHtml(myArrayMin(sampleArray), 'div17');
 
 // //#18
-// var duf = Math.max(sampleArray.every);
-// putInHtml(duf, 'div18');
+function myArrayMax(arr) {
+    return Math.max.apply(null,arr);
+}
+putInHtml(myArrayMax(sampleArray), 'div18');
 
 //#19
-
+for (i = 0; i <= 20; i++) {
+    var newElement = document.createElement("div");
+    newElement.className = "box";
+    newElement.style.backgroundColor = "grey";
+    newElement.style.height = 20 + "px";
+    newElement.style.width = 100 + "px";
+    var newText = document.createTextNode("box" + i);
+    newElement.appendChild(newText);
+    var destination = document.getElementById("div19");
+    destination.appendChild(newElement);
+}
 //#20
+for (i = 1; i <= 20; i++) {
+    var newElement = document.createElement("div");
+    newElement.className = "StepTwenty";
+    newElement.style.width = 105 + "px";
+    newElement.style.height = 20 + "px";
+    if (i % 2 == 0) {
+        newElement.style.backgroundColor = ("grey");
+    }
+  
+    var newText = document.createTextNode("box");
+    newElement.appendChild(newText);
+    var destination = document.getElementById("div20");
+    destination.appendChild(newElement);
+}
+//#21
+let width = sampleArray[i];
+for (i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        newElement.style.backgroundColor = ("grey");
+        newElement.style.width = (width + "px");
+    }
+    var newElement = document.createElement("div");
+    newElement.className = "StepTwentyOne";
+    var newText = document.createTextNode("box");
+    newElement.appendChild(newText);
+    var destination = document.getElementById("div21");
+    destination.appendChild(newElement);
+}
+//#22
+for (i = 1; i <= 20; i++) {
+    wide = sampleArray[i];
+    var newElement = document.createElement("div");
+    newElement.className = "StepTwentyTwo";
+    newElement.style.width = (wide + "px");
+    var newText = document.createTextNode("box");
+    newElement.appendChild(newText);
+    var destination = document.getElementById("div22");
+    destination.appendChild(newElement);
+    if (i % 2 == 0) {
+        newElement.style.backgroundColor = ("red");
+    }
+    // Add text into the new element //
+}
+//#23
+for (i = 1; i <= 20; i++) {
+    wide = sampleArray[i];
+    var newElement = document.createElement("div");
+    newElement.className = "StepTwentThree";
+    newElement.style.width = (wide + "px");
+    if (wide % 2 == 0) {
+        newElement.style.backgroundColor = ("red");
+    }
+    var newText = document.createTextNode("box");
+    newElement.appendChild(newText);
+    var destination = document.getElementById("div23");
+    destination.appendChild(newElement);
+}
